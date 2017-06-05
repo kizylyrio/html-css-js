18386fca978e4c7b4919f6acc4f9ec0da9079457
KizyLyrio <kizylirio@gmail.com>
2017-06-05 19:00:26 -0300

    CountDown

--- Projeto_EstouraBalões/js/game.js
+++ Projeto_EstouraBalões/js/game.js
@@ -4,6 +4,8 @@
             var parameters = queryString.split("&");
             var level = getValue(parameters[0]);
 
+            var totalBalloons = 104;
+            var burstedBallons = 0;
 
             switch (level) {
                 case "easy":
@@ -20,10 +22,12 @@
             }
 
             document.getElementById('timer').innerHTML = timeout;
+            countDown(timeout);
 
-            var totalBalloons = 20;
             document.getElementById('total-balloons').innerHTML = totalBalloons;
             createBallons(totalBalloons);
+
+            document.getElementById('bursted-ballons').innerHTML = burstedBallons;
         }
 
         function getValue(parameter) {
@@ -36,8 +40,26 @@
             for (count = 0; count < totalBalloons; count++) {
                 var balloon = document.createElement("img");
                 balloon.src = 'img/small_balloon.png';
-                balloon.style = "margin: 10px;"
+                balloon.setAttribute("class", "balloon");
 
                 document.getElementById('area').appendChild(balloon);
             }
+        }
+
+        function countDown(timeout) {
+
+            var timer = timeout;
+            var timeout = timeout * 1000; //seconds to miliseconds
+
+            //Run count down
+            var timerID = setInterval(function() {
+                timer = timer - 1;
+                document.getElementById('timer').innerHTML = timer;
+            }, 1000);
+
+            //Stop count down
+            setTimeout(function() {
+                clearInterval(timerID);
+                alert("Game Over");
+            }, timeout + 1000); //include the last second (0s);
         }
